adbb19838fa10e3bf7296a5e76e72ba65832c478
logan-stcharles <logan.fuentes.stcharles@gmail.com>
2025-09-30 07:11:20 +0000

    Ajout du script sur l'exercie B et l'exercice C.

---
+++ script.js
@@ -0,0 +1,10 @@
+let age = 17;
+
+
+if (age < 13) {
+    console.log("Accès interdit aux moins de 13 ans.");
+} else if (age < 18) {
+    console.log("Vous pouvez accéder à la section ado.");
+} else {
+    console.log("Bienvenue dans l'espace adulte.");
+}
