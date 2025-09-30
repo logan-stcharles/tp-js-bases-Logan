abae5799b3c6e6da0d7fa42a72d6b712290df925
logan-stcharles <logan.fuentes.stcharles@gmail.com>
2025-09-30 07:46:17 +0000

    feat: Exercices sur les fondamentaux JS terminés

--- script.js
+++ script.js
@@ -8,3 +8,14 @@ if (age < 13) {
 } else {
     console.log("Bienvenue dans l'espace adulte.");
 }
+
+let produit_table_de_7 = 0;
+
+for (let produit_table_de_7 = 0; produit_table_de_7 < 10;) {
+    
+    let resultat = produit_table_de_7 * 7; 
+    console.log("Calcul de " + produit_table_de_7 + " fois 7.")
+    console.log(resultat);  
+    produit_table_de_7 = produit_table_de_7 + 1;
+}
+
